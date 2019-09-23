fd4ce46aeab49a825848083ea2f42a17e2f09f72
Jad <jadizzedin@gmail.com>
2019-09-23 14:15:57 -0400

    initial commit, includes base front end structure

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Main from './components/main';
+import Header from './components/header';
+import Footer from './components/footer';
+
+
+class App extends React.Component {
+  state = {
+    
+  }
+
+  handleChange = (e) => {
+  }
+
+  componentDidMount = () => {
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      );
+    }
+  }
+
+export default App;
