ef7bd44825e0fe60d646a4d52c8cc54d0a27f4ac
Jad <jadizzedin@gmail.com>
2019-09-24 10:22:00 -0400

    add card data

---
+++ client/src/assets/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    image_url : "./images/burger.jpg",
+    title : "Burger",
+    description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+    image_url : "./images/butterSquashSoup.jpg",
+    title : "Butternut Squash Soup",
+    description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+    image_url : "./images/greekSalad.jpg",
+    title : "Greek Salad",
+    description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+    image_url : "./images/grilledCorn.jpg",
+    title : "Roasted Corn",
+    description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+  image_url : "./images/grilledCorn.jpg",
+  title : "Grilled Corn",
+  description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+  image_url : "./images/mSalad.jpg",
+  title : "Mediterranean Salad",
+  description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+  image_url : "./images/pumpkin.jpg",
+  title : "Pumpkin",
+  description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+  image_url : "./images/basilRainbowSalad.jpg",
+  title : "Basil Rainbow Salad",
+  description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+  image_url : "./images/tapasAndSides.jpg",
+  title : "Tapas and Sides",
+  description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  },
+  {
+  image_url : "./images/truffles.jpg",
+  title : "Truffles",
+  description : "Viverra a lectus sed sit morbu in et consectetur risus viverra a lectus sed sit morbi in et tetur risus",
+  }
+]
+
+module.exports = data;
