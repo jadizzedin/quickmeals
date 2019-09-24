50058252b5ecff8c3febbc001e4713b1140344de
mfhan <mf212mf@gmail.com>
2019-09-24 12:07:52 -0400

    started header styling

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,9 +1,27 @@
 import React from "react";
+import './header.css'
+
 
 function Header(props) {
   return (
     <div className="header">
-        <h1>HEADER</h1>
+
+        <img className="header-logo"  src="assets/xxxx"/>
+
+        <div className="web-input">
+          <input className="input"  type="text" placeholder="Keywords" />
+          <button className="input"  id="filter" type="text" placeholder="Filter"/>
+        </div>
+
+        <div className="right-side">
+          <i className="fas fa-cart-plus"></i>&nbsp;
+          <i className="fas fa-search"></i>&nbsp;
+          <span className="element">About</span>&nbsp;
+          <span className="element">Saved</span>&nbsp;
+          <span className="element" >Settings</span>&nbsp;
+          <span className="element" >Checkout</span>
+        </div>
+
     </div>
   )
 }
